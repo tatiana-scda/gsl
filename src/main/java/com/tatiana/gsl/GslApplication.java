711792af25bcb4f61a093b15823ee89bc9bf582e
Tatiana Camelo <t.camelo.ext@planerio.de>
2021-10-18 21:35:23 -0300

    Inicial commit

---
+++ src/main/java/com/tatiana/gsl/GslApplication.java
@@ -0,0 +1,13 @@
+package com.tatiana.gsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatiana/gsl/GslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatiana.gsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
