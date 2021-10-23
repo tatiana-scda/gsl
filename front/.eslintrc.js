499d1aba9182474936bdb5d7a8e82d89d4efe7f7
Tatiana Camelo <t.camelo.ext@planerio.de>
2021-10-23 13:15:03 -0300

    Front inicial commit

---
+++ front/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'eol-last': [
+      'error',
+      'always',
+    ],
+    'indent': [
+      'error',
+      2,
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+  },
+}
