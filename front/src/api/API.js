bf84f72c74da0e48ad726e4c86525cd09a37442e
Tatiana Camelo <t.camelo.ext@planerio.de>
2021-10-28 14:06:14 -0300

    URL e headers

--- front/src/api/API.js
+++ front/src/api/API.js
@@ -1,15 +1,17 @@
 export default class API {
-  static #base = 'https://c44a-2804-14c-5b81-46c6-55ea-5d8e-ba83-9008.ngrok.io/gsl-service/v1'
+  static #base = 'http://localhost:8080/gsl-service/v1'
 
   static async #fetch(url, method, body = undefined) {
     return new Promise((resolve, reject) => {
       fetch(
         `${API.#base}${url}`,
         {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
           method,
           ...(body !== undefined && { body: JSON.stringify(body) }),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
         },
       ).then(
         response => resolve(response.json()),
